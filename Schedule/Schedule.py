e4d0a91ab3b85199cf05f9b16b9792600e1026c4
මම මේ දේට ආදරෙයි <amajidi@browning.edu>
2026-02-27 19:37:35 +0000

    Commit

---
+++ Schedule/Schedule.py
@@ -0,0 +1,55 @@
+Monday = input("Monday Red or Monday Black? ").lower()
+
+if Monday == "Monday Red":
+    print("Monday:")
+    print("Dialogue")
+    print("Assembly")
+    print("Chemistry")
+    print("Lunch")
+    print("Algebra")
+else: 
+    print("Monday:")
+    print("English")
+    print("French")
+    print("History")
+    print("Lunch")
+    print("Research Principles")
+
+print("\nTuesday:")
+print("Chemistry")
+print("Algebra")
+print("CSE")
+print("Lunch")
+print("Art")
+print("French")
+print("English")
+
+
+print("\n Wednesday:")
+print("History")
+print("DIalogue")
+print("French")
+print("Lunch")
+print("Algebra")
+print("Chemistry")
+
+
+print("\n Thursday:")
+print("Algebra")
+print("English")
+print("Research Principles")
+print("Lunch")
+print("Chemistry")
+print("Community Time")
+print("History")
+
+print("\n Friday:")
+print("French")
+print("Art")
+print("English")
+print("Lunch")
+print("History")
+print("Community Time")
+print("CSE")
+
+
